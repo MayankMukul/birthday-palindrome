543f4adf2cc3236713bb69134137b95ca51574a6
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-01 12:49:20 +0530

    is palindrome

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var dob = document.querySelector("#dob");
 var string = document.querySelector("#string");
 var btn = document.querySelector("#submit");
+var message = document.querySelector("#output");
 
 function reverseStr (str) {
     var chrlst=str.split('');
@@ -8,18 +9,31 @@ function reverseStr (str) {
     return (rev.join(''));
 }
 
-function process (){
+function ispalindrome (){
     var revStr = reverseStr(string.value);
     console.log(`Original String: ${string.value}`);
     console.log(`Reverse of the given string is :${revStr}`) ;
-    // var j = string.length;
-    // for (var i=0;i<j;i++) {
-    //     if (string.value[i]===revStr[i]) {
-    //         console.log(string.value[i]);
-    //     } else {
-    //         console.log("failed");
-    //     }
-    // }
+    
+    
+    for (var i=0 ; i<string.value.length/2 ; i++) {
+        if (string.value[i]===revStr[i]) {
+        } else {
+            message.innerText ="String is Not Palindrome";
+            return  ; 
+        }
+    }
+    console.log("String is a palindrome");
+    message.innerText = "String is Palindrome";
 }
 
-btn.addEventListener("click",process);
+// function process(){
+//     var bool = ispalindrome;
+//     console.log(bool);
+//     if(bool === "true"){
+//         message.innerText = "String is Palindrome";
+//     } else {
+//         message.innerText ="String is Not Palindrome";
+//     }
+//}
+
+btn.addEventListener("click",ispalindrome);
